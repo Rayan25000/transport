1b6fc475338503bda94cd2cda1d284e2f3b411e4
roudbib <rayan.oudbib@etud.univ-jfc.fr>
2023-02-10 15:03:39 +0100

    Init

---
+++ src/main/java/fr/isis/transport/dao/ExamenRepository.java
@@ -0,0 +1,9 @@
+package fr.isis.transport.dao;
+
+import fr.isis.transport.entity.ExamenEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExamenRepository extends JpaRepository<ExamenEntity, Integer> {
+
+
+}
